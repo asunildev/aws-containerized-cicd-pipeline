31b8463456ff8d90f82b7c5cb322ca588839ad38
Ananthu S <asunil.official02@gmail.com>
2026-06-27 15:41:10 +0530

    GitHub Actions Deployment to Elastic BeanStalk

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload ok i will do.
+          Edit <code>src/App.js</code> Welcome to your first deployment.
         </p>
         <a
           className="App-link"
